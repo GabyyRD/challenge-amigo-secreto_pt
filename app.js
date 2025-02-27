039aa29e98a5650ae1a5511f6105153864fa5034
Gabrielly <gabyrosariod@gmail.com>
2025-02-26 21:27:23 -0300

    feat: adicionando nomes à lista e limpando campo input

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 
 let amigos = []
+
+function adicionarAmigo() {
+    let entrada = document.querySelector('input').value;
+
+    if (entrada.trim() !== "") {
+        console.log("Entrada válida:", entrada);
+        amigos.push(entrada);
+    } else {
+        alert("Por favor, insira um nome.");
+    }
+
+    limparCampo();
+}
+
+function limparCampo() {
+    entrada = document.querySelector('input');
+    entrada.value = '';
+}
+
